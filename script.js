0b6adb2fab5fa30ef66180a43cc54640cac776d6
MuneLiesWithin <romulo_medeiros@hotmail.com>
2025-01-07 17:30:40 -0300

    first

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿function uploadExcel() {
+    console.log("TESTE...")
+}
+
+function flashMessage(type, message) {
+    const messageElement = document.getElementById("message");
+    messageElement.classList.add(type);
+    messageElement.innerHTML = message;
+    setTimeout(() => {
+        messageElement.innerHTML = "";
+        messageElement.classList.remove(type);
+    }, 3000);
+}
+
+function handleKeyPress(event, buttonId) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); 
+        document.getElementById(buttonId).click(); 
+    }
+}
